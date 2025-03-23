702dad354f6e94098aef9986f282bf61f80bd8de
lg <lg@lgreve.com>
2025-03-23 11:22:10 +1100

    fix build locally

--- AsxDbScraper/Program.cs
+++ AsxDbScraper/Program.cs
@@ -1,8 +1,30 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using AsxDbScraper.Services;
+using AsxDbScraper.Data;
+using Microsoft.EntityFrameworkCore;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(Startup.ConfigureServices)
+    .ConfigureServices((context, services) =>
+    {
+        // Configure Serilog
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(context.Configuration)
+            .CreateLogger();
+
+        // Register services
+        services.AddLogging(loggingBuilder =>
+            loggingBuilder.AddSerilog(dispose: true));
+
+        services.AddHttpClient();
+        services.AddScoped<IAlphaVantageService, AlphaVantageService>();
+        services.AddScoped<IAsxCompanyService, AsxCompanyService>();
+        services.AddDbContext<AsxDbContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+    })
     .Build();
 
-await host.RunAsync(); 
+await host.RunAsync();
